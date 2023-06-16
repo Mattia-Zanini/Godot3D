248a4249038ccfb2b52a9ddfdeaf0e390b9b510e
Mattia-Zanini <mattzanini2@gmail.com>
2023-06-17 00:03:39 +0200

    added 3D model and animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -3,8 +3,17 @@ extends CharacterBody3D
 var speed
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
+var isRunning = false
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.01
+const MAX_VIEW_ANGLE = 60
+const MIN_VIEW_ANGLE = -45
+@export var SENSITIVITY_VERTICAL = 0.01
+@export var SENSITIVITY_HORIZONTAL = 0.5
+var inGame = true
+
+#only for "PlayerTest" model
+const HEAD_WALKING = Vector3(-0.02, 1.656, -0.196)
+const HEAD_RUNNING = Vector3(-0.02, 1.59, -0.36)
 
 # Bobbing variables
 const BOB_FREQ = 2.0
@@ -21,19 +30,29 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var animation_player = $visuals/model/AnimationPlayer
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # function called every time up where the player does anything like press a button or move the mouse
 func _unhandled_input(event):
+	if Input.is_action_just_pressed("ui_escape"):
+		inGame = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		inGame = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	# check if the event is a mouse motion event
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion && inGame == true:
 		# change the rotation of the head depending on how much the mouse moved relatively multiplied by the sensitivity
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		#head.rotate_y(-event.relative.x * SENSITIVITY_HORIZONTAL)
+		rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY_HORIZONTAL))
+		camera.rotate_x(-event.relative.y * SENSITIVITY_VERTICAL)
 		# the clamp function limit the rotation of the camera to a minimum and a maximum value
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(60))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(MIN_VIEW_ANGLE), deg_to_rad(MAX_VIEW_ANGLE))
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -41,24 +60,39 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
+	"""
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	"""
 	
 	# Handle player sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
+		isRunning = true
 	else:
 		speed = WALK_SPEED
+		isRunning = false
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	# the direction is based on where the head is direct
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
+			if animation_player.current_animation != "walking" || animation_player.current_animation != "running":
+				if isRunning == true:
+					animation_player.play("running")
+					head.position = HEAD_RUNNING
+				else:
+					animation_player.play("walking")
+					head.position = HEAD_WALKING
+			
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
+			if animation_player.current_animation != "idle":
+				animation_player.play("idle")
+			
 			velocity.x = 0.0
 			velocity.z = 0.0
 	else:
